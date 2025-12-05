c0879458a2d2554351321bac596764eb1ef47535
Tristan Goehring <goehrint@oregonstate.edu>
2025-12-05 13:57:13 -0800

    worked on footer

--- app/page.tsx
+++ app/page.tsx
@@ -25,19 +25,37 @@ export default function Home() {
       </section>
 
       <footer
-        className="flex h-[60dvh] flex-col"
+        className="flex h-[60dvh] justify-between"
         style={{ backgroundImage: "url('/bg.png')", backgroundSize: "cover" }}
       >
-        <div className="mt-5 flex flex-col items-end gap-5">
+        <div>
+          <div className="flex flex-col justify-start">
+            <div className="reliative">
+              <div className="relative flex flex-col mt-35">
+                <img src="/banner-main.png" alt="" className="w-125 " /> 
+                <img src="/banner-image.png" alt="" className=" bottom-0 left-0 absolute w-125 " />
+              </div>
+            </div>
+            <div className="flex items-center justify-start bg-[#1D1357]/50 rounded-br-md w-125 h-16">
+               <div className="text-white text-2md ml-10">MEET THE TEAM</div>
+               <div className="relative flex flex-col">
+                <div className="flex items-center ml-30 rounded-br-md rounded-tr-md justify-start bg-[#1D1357]/50 rounded-br-md w-30 h-10"></div>
+                <div className="text-white text-2md ml-10">15000</div>
+               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5 mb-5 flex flex-col justify-end gap-5">
           <div className="relative">
             <img src="/banner1.png" alt="" className="w-100 scale-x-[-1]" />
             <div className="absolute inset-y-0 left-20 flex text-white">
-              <div className="my-auto text-2xl [text-shadow:_0_0_5px_green]">SEAN GUTMANN</div>
+              <div className="my-auto text-2xl [text-shadow:_0_0_5px_green] ">SEAN GUTMANN</div>
             </div>
           </div>
 
           <div className="relative">
-            <img src="/banner5.png" alt="" className="w-100" />
+            <img src="/banner4.png" alt="" className="w-100" />
             <div className="absolute inset-y-0 left-20 flex text-white">
               <div className="my-auto text-2xl [text-shadow:_0_0_5px_purple]">TRISTAN GOEHRING</div>
             </div>
@@ -53,7 +71,7 @@ export default function Home() {
           <div className="relative">
             <img src="/banner3.png" alt="" className="w-100" />
             <div className="absolute inset-y-0 left-20 flex text-white">
-              <div className="my-auto text-2xl drop-shadow(30px 10px 4px orange); uppercase">ETHAN BENTLEY</div>
+              <div className="my-auto text-2xl [text-shadow:_0_0_5px_red] uppercase">ETHAN BENTLEY</div>
             </div>
           </div>
         </div>
